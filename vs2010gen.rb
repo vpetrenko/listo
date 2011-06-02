0ce6ea85319f7eb872867d5e8c0b9533c79f1d88
Developer <sample@sample.com>
2011-06-02 07:35:38 +0000

    IMPLEMENTED: #208(Генерация проектов для VS2010)
    git-svn-id: https://opsgroup.ru:8080/svn/Ops5/trunk@3330 2950fbd3-dd18-dd11-99f3-001e4f9cf9ff

--- vs2010gen.rb
+++ vs2010gen.rb
@@ -4,21 +4,14 @@ require 'project'
 require 'uuid'
 require 'builder'
 
-##
-# QT Mocs
-#%QTDIR%/qmake/generators/makefiledeps.cpp
-#"Q_"
-#"QOM_"
-#"OBJECT", "GADGET", "M_OBJECT"
-##
-
-
 class VS2010Generator
   def initialize(project)
     @project = project
     @xml = nil
     @generated_files = {}
     @current_storage = nil
+	@filters = {}
+	@filter_props = {}
   end
 
   def prepare_files(sources)
@@ -51,16 +44,18 @@ class VS2010Generator
     false
   end
 
-  def generate_file(file_path)
+  def generate_file(file_path, sub_path)
     if File.extname(file_path) == '.cpp'
-	  @xml.ClCompile :Include => decorate_path(file_path)
+	  @xml.ClCompile :Include => file_path
+	  @filters[file_path] = {'Filter' => "Source Files#{sub_path}", 'Type' => 'ClCompile'}
     end
 
 	if File.extname(file_path) == '.h'
 	  if !need_moc(file_path)
-	    @xml.ClInclude :Include => decorate_path(file_path)
+	    @xml.ClInclude :Include => file_path
+		@filters[file_path] = {'Filter' => "Header Files#{sub_path}", 'Type' => 'ClInclude'}
 	  else
-	    @xml.CustomBuild :Include => decorate_path(file_path) do
+	    @xml.CustomBuild :Include => file_path do
           @project.configurations.each_value do |config|
             storage = ConstStorage.new
             flags = config.flags
@@ -70,47 +65,76 @@ class VS2010Generator
             storage.fill(config.actions, flags)
             World.postprocess_storage(@project, config, storage)
 			
+			generated_file = "#{config.name}\\moc_#{File.basename(file_path, '.h')}.cpp"
 			condition = "'$(Configuration)|$(Platform)'=='#{config.name}|Win32'"
 			@xml.Message({:Condition => condition}, "MOC #{File.basename(file_path)}")
-			@xml.Outputs({:Condition => condition}, "#{config.name}\\moc_#{File.basename(file_path, ".h")}.cpp;%(Outputs)")
-			@xml.AdditionalInputs({:Condition => condition}, "#{decorate_path(file_path)};#{qt_moc};%(AdditionalInputs)")
+			@xml.Outputs({:Condition => condition}, "#{generated_file};%(Outputs)")
+			@xml.AdditionalInputs({:Condition => condition}, "#{file_path};#{qt_moc};%(AdditionalInputs)")
 			@xml.Command({:Condition => condition}, "#{qt_moc} #{storage.get_values(Maker::DEFINES, ' ', '-D')}" \
               " #{storage.get_decorated_paths(Maker::INCLUDE_DIRS, ' ', '-I', '', '"')}" \
-              " #{decorate_path(file_path)} -o #{config.name}\\moc_#{File.basename(file_path, '.h')}.cpp")
+              " #{file_path} -o #{config.name}\\moc_#{File.basename(file_path, '.h')}.cpp")
             @generated_files[config.name] = [] unless @generated_files.key?(config.name)
-            @generated_files[config.name] << "#{config.name}\\moc_#{File.basename(file_path, '.h')}.cpp"
+            @generated_files[config.name] << generated_file
+			@filters[generated_file] = {'Filter' => 'Generated Files', 'Type' => 'ClCompile'}
           end
 		end
+		@filters[file_path] = {'Filter' => "Header Files#{sub_path}", 'Type' => 'CustomBuild'}
 	  end
     end
   end
 
-  def gen_files(xml, project, paths, parent = '')
+  def gen_files(xml, project, paths, sub_path = '')
     if paths == nil
       return
     end
 
     if paths.length == 1 && !paths.first[1].is_a?(String)
-      gen_files(xml, project, paths.first[1], parent)
+      gen_files(xml, project, paths.first[1], sub_path)
       return
     end
 
     paths.each do |k, v|
       if v.is_a? String
-        generate_file(v)
+        generate_file(v, sub_path)
       end
     end
-    if parent == ''
-      paths.each do |k, v|
-        if v.is_a? Hash
-          gen_files(xml, project, v, k)
-        end
+
+	paths.each do |k, v|
+      if v.is_a? Hash
+        gen_files(xml, project, v, sub_path + '\\' + k)
       end
     end
   end
+  
+  def generate_filters(project, project_file_name)
+    File.open(project_file_name + ".filters", "w") do |file|
+      xml = Builder::XmlMarkup.new(:indent => 2, :target => file)
+      xml.instruct!
+    
+	  xml.Project :ToolsVersion => '4.0', :xmlns => 'http://schemas.microsoft.com/developer/msbuild/2003' do
+	    xml.ItemGroup do
+		  @filters.each_value {|val| @filter_props[val['Filter']] = {} unless @filter_props.has_key?(val['Filter'])}
+		  @filter_props.each do |filter,props|
+		    xml.Filter :Include => filter do
+			  props.each do |key,val|
+			    xml.__send__(key.to_sym, val)
+			  end
+			end
+		  end
+		end
+	    xml.ItemGroup do
+		  @filters.each do |file,desc|
+		    xml.__send__(desc['Type'], :Include => file) do
+			  xml.Filter desc['Filter']
+			end
+		  end		  
+		end
+	  end
+	end
+  end
 
-  def generate_project(project, base_file_name)
-    File.open(base_file_name + ".vcxproj", "w") do |file|
+  def generate_project(project, file_name)
+    File.open(file_name, "w") do |file|
       storage = ConstStorage.new
       storage.fill(project.actions, project.flags)
       @current_storage = storage
@@ -228,24 +252,29 @@ class VS2010Generator
         storage.fill(project.actions, project.flags)
 
 		if storage.has?(Maker::FILES_UI)
+		  @filter_props['Form Files'] = {'UniqueIdentifier' => '{99349809-55BA-4b9d-BF79-8FDBB0286EB3}', 'Extensions' => 'ui'}
           xml.ItemGroup do
 		    storage.get_array(Maker::FILES_UI).each do |ui|
               xml.CustomBuild :Include => ui do
                 @project.configurations.each_value do |conf|
 			      condition = "'$(Configuration)|$(Platform)'=='#{conf.name}|Win32'"
+				  generated_file = "ui_#{File.basename(ui, '.ui')}.h"
 				  xml.Message({:Condition => condition}, "UIC #{ui}")
 				  xml.Outputs({:Condition => condition}, "ui_#{File.basename(ui, '.ui')}.h;%(Outputs)")
 				  xml.AdditionalInputs({:Condition => condition}, "#{ui};#{qt_bin_path}\\uic.exe;%(AdditionalInputs)")
-				  xml.Command({:Condition => condition}, "#{qt_bin_path}\\uic.exe #{ui} -o ui_#{File.basename(ui, '.ui')}.h")
+				  xml.Command({:Condition => condition}, "#{qt_bin_path}\\uic.exe #{ui} -o #{generated_file}")
                   @generated_files[conf.name] = [] unless @generated_files.key?(conf.name)
-                  @generated_files[conf.name] << "ui_#{File.basename(ui, '.ui')}.h"
+                  @generated_files[conf.name] << generated_file
+				  @filters[generated_file] = {'Filter' => 'Generated Files', 'Type' => 'ClInclude'}
                 end
+				@filters[ui] = {'Filter' => 'Form Files', 'Type' => 'CustomBuild'}
               end
 			end
 		  end
         end
 
 		xml.ItemGroup do
+		  @filter_props['Header Files'] = {'UniqueIdentifier' => '{93995380-89BD-4b04-88EB-625FBE52EBFB}', 'Extensions' => 'h;hpp;hxx;hm;inl;inc;xsd'}
 		  gen_files(xml, project, prepare_files(storage.get_array(Maker::FILES_H))) if storage.has?(Maker::FILES_H)
           if !@generated_files.empty?
             @generated_files[@generated_files.keys[0]].each do |path|
@@ -259,29 +288,35 @@ class VS2010Generator
         end
 		
         if storage.has?(Maker::FILES_QRC)
+		  @filter_props['Resource Files'] = {'UniqueIdentifier' => '{D9D6E242-F8AF-46E4-B9FD-80ECBC20BA3E}', 'Extensions' => 'qrc;*'}
 		  xml.ItemGroup do
 		    storage.get_array(Maker::FILES_QRC).each do |qrc|
 			  xml.CustomBuild :Include =>qrc do
 			    @project.configurations.each_value do |conf|
 				  condition = "'$(Configuration)|$(Platform)'=='#{conf.name}|Win32'"
 				  resource_files = ''
+				  generated_file = "#{conf.name}\\qrc_#{File.basename(qrc, '.qrc')}.cpp"
                   storage.get_array(Maker::FILES_RESOURCES).each {|r| resource_files += r + ';'} if storage.has?(Maker::FILES_RESOURCES)
 				  xml.Message({:Condition => condition}, "RCC #{qrc}")
 				  xml.Outputs({:Condition => condition}, "#{conf.name}\\qrc_#{File.basename(qrc, '.qrc')}.cpp;%(Outputs)")
 				  xml.AdditionalInputs({:Condition => condition}, "#{qrc};#{qt_bin_path}\\rcc.exe;#{resource_files};%(AdditionalInputs)")
-				  xml.Command({:Condition => condition}, "#{qt_bin_path}\\rcc.exe -name #{@project.name} #{qrc} -o #{conf.name}\\qrc_#{File.basename(qrc, '.qrc')}.cpp")
+				  xml.Command({:Condition => condition}, "#{qt_bin_path}\\rcc.exe -name #{@project.name} #{qrc} -o #{generated_file}")
 				  @generated_files[conf.name] = [] unless @generated_files.key?(conf.name)
-				  @generated_files[conf.name] << "#{conf.name}\\qrc_#{File.basename(qrc, '.qrc')}.cpp"
+				  @generated_files[conf.name] << generated_file
+				  @filters[generated_file] = {'Filter' => 'Generated Files', 'Type' => 'ClCompile'}
 				end
+				@filters[qrc] = {'Filter' => 'Resource Files', 'Type' => 'CustomBuild'}
 			  end
 			end
 		  end
 		end
 		
 		xml.ItemGroup do
+		  @filter_props['Source Files'] = {'UniqueIdentifier' => '{4FC737F1-C7A5-4376-A066-2A32D752A2FF}', 'Extensions' => 'cpp;c;cc;cxx;def;odl;idl;hpj;bat;asm;asmx'}
 		  gen_files(xml, project, prepare_files(storage.get_array(Maker::FILES_CPP))) if storage.has?(Maker::FILES_CPP)
  
           if !@generated_files.empty?
+		    @filter_props['Generated Files'] = {'UniqueIdentifier' => '{71ED8ED8-ACB9-4CE9-BBE1-E00B30144E11}', 'Extensions' => 'cpp;c;cxx;moc;h;def;odl;idl;res'}
             @generated_files.each_pair do |config, files|
               files.each do |path|
 			    if File.extname(path) != '.h'
@@ -309,8 +344,10 @@ class VS2010Generator
         xml.ImportGroup :Label=> 'ExtensionTargets'
 	  end
 	end
+	
+	generate_filters(project, file_name)
 
-    puts "MSVC Project '#{project.name}' generated at '#{base_file_name}'"
+    puts "MSVC Project '#{project.name}' generated at '#{file_name}'"
   end
 
 end
